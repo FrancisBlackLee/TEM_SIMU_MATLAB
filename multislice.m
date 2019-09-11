7ead888b665056e77578aec2d3fe14e591946b38
F. B. Lee <warner323@outlook.com>
2019-09-11 11:19:28 +0800

    some fixes
    change the saving mode of multislice function, for specific usage please refer to DPC_nvstgt_0.m under DPC folder.
    A manual to be released soon.

--- multislice.m
+++ multislice.m
@@ -1,4 +1,4 @@
-function [ExitWave] = multislice(IncidentWave, WaveLength, Lx, Ly, TransFuncs, LayerDist, StackNum, WaveFileName, SavePrec)
+function [ExitWave] = multislice(IncidentWave, WaveLength, Lx, Ly, TransFuncs, LayerDist, StackNum, SaveDir, SaveSliceSeries)
 %MULTISLICE.M performs the multislice procedure. See E. J. Kirkland
 %Advanced Computing in Electron Microscopy.
 %   IncidentWave;
@@ -6,15 +6,12 @@ function [ExitWave] = multislice(IncidentWave, WaveLength, Lx, Ly, TransFuncs, L
 %   TransFuncs -- transmission functions;
 %   LayerDist -- distance from one layer to the next layer;
 %   StackNum -- number of stackings;
-%   WaveFileName -- optional: save the wave function on each slice
-%       including the incident wave function and exit wave function, each
-%       wave function is save as two lines with input precision in the
-%       destination txt file, real component for one line and imaginary
-%       component for the next line, respectively. Notice that each line
-%       represents a single matrix which is rearranged by columns and then
-%       saved as one line;
-%   SavePrec -- half optional: if the WaveFileName is input, SavePrec must
-%       be given;
+%   SaveDir -- the directory where you want these wave functions to be
+%       saved;
+%   SaveSliceSeries -- Slice indices upon which the wave functions are to
+%       be saved, i.e. [1, 2, 6, 8, 12, 34], note that the array should be
+%       in an increasing order and it maximum element should be no greater
+%       than N + 1, where N = StackNum * LayerNum;
 
 switch nargin
     case 7
@@ -28,26 +25,41 @@ switch nargin
         end
         ExitWave = Wave;
     otherwise
-        Prec = strcat('%.', num2str(SavePrec), 'f  \t');
-        fileID = fopen(WaveFileName, 'w');
+        MkDirStat = mkdir(SaveDir);
+        SaveSliceIdx = 1;
+        SaveSliceNum = length(SaveSliceSeries);
         Wave = IncidentWave;
-        fprintf(fileID, Prec, real(Wave));
-        fprintf(fileID, '\r\n');
-        fprintf(fileID, Prec, imag(Wave));
-        fprintf(fileID, '\r\n');
         LayerNum = length(LayerDist);
         for i = 1: StackNum
             for j = 1: LayerNum
+                SliceIdx = (i - 1) * LayerNum + j;
+                if SaveSliceIdx <= SaveSliceNum
+                    if SliceIdx == SaveSliceSeries(SaveSliceIdx)
+                        FileNameReal = strcat(SaveDir, '\', num2str(SliceIdx), '_real.txt');
+                        FileNameImag = strcat(SaveDir, '\', num2str(SliceIdx), '_imag.txt');
+                        WaveReal = real(Wave);
+                        WaveImag = imag(Wave);
+                        save(FileNameReal, 'WaveReal', '-ascii', '-double', '-tabs');
+                        save(FileNameImag, 'WaveImag', '-ascii', '-double', '-tabs');
+                        SaveSliceIdx = SaveSliceIdx + 1;
+                    end
+                end
                 Wave = Wave .* TransFuncs(:, :, j);
                 Wave = propTF_1(Wave, Lx, Ly, WaveLength, LayerDist(j));
-                fprintf(fileID, Prec, real(Wave));
-                fprintf(fileID, '\r\n');
-                fprintf(fileID, Prec, imag(Wave));
-                fprintf(fileID, '\r\n');
+            end
+        end
+        if SaveSliceIdx <= SaveSliceNum
+            if SaveSliceSeries(SaveSliceIdx) == SliceIdx + 1
+                FileNameReal = strcat(SaveDir, '\', num2str(SliceIdx+1), '_real.txt');
+                FileNameImag = strcat(SaveDir, '\', num2str(SliceIdx+1), '_imag.txt');
+                WaveReal = real(Wave);
+                WaveImag = imag(Wave);
+                disp([SliceIdx + 1, SaveSliceSeries(SaveSliceIdx)]);
+                save(FileNameReal, 'WaveReal', '-ascii', '-double', '-tabs');
+                save(FileNameImag, 'WaveImag', '-ascii', '-double', '-tabs');
             end
         end
         ExitWave = Wave;
-        fclose(fileID);
 end
 
 end
