dd10d32404cb7f2f14be8215f7dd60e1892e6225
F. B. Lee <warner323@outlook.com>
2019-11-02 23:24:35 +0800

    Update CrystalSlicing_X.m

--- CrystalSlicing/CrystalSlicing_X.m
+++ CrystalSlicing/CrystalSlicing_X.m
@@ -1,4 +1,4 @@
-function [slice, SliceDist, ExtraSlice] = CrystalSlicing_X(StdLatt, ThermoLatt, DistError, zMax, YN, PlotColor)
+function [slice, SliceDist, ExtraSlice] = CrystalSlicing_X(StdLatt, ThermoLatt, DistError, zMax, LattMode, YN, PlotColor)
 %CrystalSlicing_X.m slices a given crystal described by the atomic numbers
 %and atomic coordinates.
 %   StdLatt -- standard lattice matrix (without thermo-displacements), uses
@@ -11,8 +11,16 @@ function [slice, SliceDist, ExtraSlice] = CrystalSlicing_X(StdLatt, ThermoLatt, 
 %       different heights be rearranged to one slice;
 %   YN -- whether to show each slice: 1 --yes, 0 --no.
 %   PlotColor -- colors for each type of atom;
+%   LattMode -- lattice mode:
+%       LattMode == 0 : cubic specimen;
+%       LattMode == 1: nanoparticle;
 %   NOTE: X denotes an experimental version!
 %   Current target: lattice with thermo-displacements.
+
+AtomNum = size(StdLatt, 2);
+if nargin == 6
+    PlotColor = ones(1, AtomNum);
+end
 
 [zStd, zOrder] = sort(StdLatt(5, : ));
 StdLatt = StdLatt( : , zOrder);
@@ -39,59 +47,81 @@ for zIdx = 2 : length(zStd)
 end
 % determine whether the last slice should be preserved, deleted or counted
 % as the first slice:
-if zMax - sum(SliceDist) >= DistError
+if LattMode == 1
     SliceDist(SliceNum) = zMax - sum(SliceDist);
     ExtraSlice = 0;
+    xmin = min(StdLatt(3, : )) - 1;
+    xmax = max(StdLatt(3, : )) + 1;
+    ymin = min(StdLatt(4, : )) - 1;
+    ymax = max(StdLatt(4, : )) + 1;
+    if YN == 1
+        for SliceIdx = 1 : SliceNum
+            ColorList = ['r', 'g', 'b', 'y', 'm', 'c', 'w', 'k'];
+            figure;
+            hold on;
+            for AtomIdx = 1 : size(slice{SliceIdx}, 2)
+                scatter(slice{SliceIdx}(3, AtomIdx), slice{SliceIdx}(4, AtomIdx), 'o', ColorList(color{SliceIdx}(AtomIdx)));
+            end
+            axis([xmin xmax ymin ymax]);
+            axis equal;
+            title(['slice ', num2str(SliceIdx)]);
+        end
+    end
 else
-    % rearrange the first slice and last slice by ascending order of x,
-    % then y, so that whether the last slice is identical to the first
-    % slice could be determined
-    IdMinDist = 1e-2;
-    IdCrit = 0;
-    for TopIdx = 1 : size(slice{1}, 2)
-        for BotIdx = 1 : size(slice{SliceNum}, 2)
-            TempDist = sqrt(sum((slice{1}(3:4, TopIdx) - slice{SliceNum}(3:4, BotIdx)).^2));
-            if (TempDist <= IdMinDist) && (slice{1}(1, TopIdx) == slice{4}(1, BotIdx))
-                IdCrit = 1;
+    if zMax - sum(SliceDist) >= DistError
+        SliceDist(SliceNum) = zMax - sum(SliceDist);
+        ExtraSlice = 0;
+    else
+        % rearrange the first slice and last slice by ascending order of x,
+        % then y, so that whether the last slice is identical to the first
+        % slice could be determined
+        IdMinDist = 1e-2;
+        IdCrit = 0;
+        for TopIdx = 1 : size(slice{1}, 2)
+            for BotIdx = 1 : size(slice{SliceNum}, 2)
+                TempDist = sqrt(sum((slice{1}(3:4, TopIdx) - slice{SliceNum}(3:4, BotIdx)).^2));
+                if (TempDist <= IdMinDist) && (slice{1}(1, TopIdx) == slice{4}(1, BotIdx))
+                    IdCrit = 1;
+                    break;
+                end
+            end
+            if IdCrit == 1
                 break;
             end
         end
         if IdCrit == 1
-            break;
+            ExtraSlice = slice{SliceNum};
+            SliceDist(SliceNum - 1) = SliceDist(SliceNum - 1) + zMax - sum(SliceDist);
+            slice(SliceNum) = [];
+            color(SliceNum) = [];
+            SliceNum = SliceNum - 1;
+        else
+            ExtraSlice = 0;
+            SliceDist(SliceNum - 1) = SliceDist(SliceNum - 1) + zMax - sum(SliceDist);
+            slice{SliceNum}(5, : ) = zStd(1);
+            slice{1} = [slice{1}, slice{SliceNum}];
+            color{1} = [color{1}, color{SliceNum}];
+            slice(SliceNum) = [];
+            color(SliceNum) = [];
+            SliceNum = SliceNum - 1;
         end
     end
-    if IdCrit == 1
-        ExtraSlice = slice{SliceNum};
-        SliceDist(SliceNum - 1) = SliceDist(SliceNum - 1) + zMax - sum(SliceDist);
-        slice(SliceNum) = [];
-        color(SliceNum) = [];
-        SliceNum = SliceNum - 1;
-    else
-        ExtraSlice = 0;
-        SliceDist(SliceNum - 1) = SliceDist(SliceNum - 1) + zMax - sum(SliceDist);
-        slice{SliceNum}(5, : ) = zStd(1);
-        slice{1} = [slice{1}, slice{SliceNum}];
-        color{1} = [color{1}, color{SliceNum}];
-        slice(SliceNum) = [];
-        color(SliceNum) = [];
-        SliceNum = SliceNum - 1;
-    end
-end
-xmin = min(StdLatt(3, : )) - 1;
-xmax = max(StdLatt(3, : )) + 1;
-ymin = min(StdLatt(4, : )) - 1;
-ymax = max(StdLatt(4, : )) + 1;
-if YN == 1
-    for SliceIdx = 1 : SliceNum
-        ColorList = ['r', 'g', 'b', 'y', 'm', 'c', 'w', 'k'];
-        figure;
-        hold on;
-        for AtomIdx = 1 : size(slice{SliceIdx}, 2)
-            scatter(slice{SliceIdx}(3, AtomIdx), slice{SliceIdx}(4, AtomIdx), 'o', ColorList(color{SliceIdx}(AtomIdx)));
+    xmin = min(StdLatt(3, : )) - 1;
+    xmax = max(StdLatt(3, : )) + 1;
+    ymin = min(StdLatt(4, : )) - 1;
+    ymax = max(StdLatt(4, : )) + 1;
+    if YN == 1
+        for SliceIdx = 1 : SliceNum
+            ColorList = ['r', 'g', 'b', 'y', 'm', 'c', 'w', 'k'];
+            figure;
+            hold on;
+            for AtomIdx = 1 : size(slice{SliceIdx}, 2)
+                scatter(slice{SliceIdx}(3, AtomIdx), slice{SliceIdx}(4, AtomIdx), 'o', ColorList(color{SliceIdx}(AtomIdx)));
+            end
+            axis([xmin xmax ymin ymax]);
+            axis equal;
+            title(['slice ', num2str(SliceIdx)]);
         end
-        axis([xmin xmax ymin ymax]);
-        axis equal;
-        title(['slice ', num2str(SliceIdx)]);
     end
 end
 
