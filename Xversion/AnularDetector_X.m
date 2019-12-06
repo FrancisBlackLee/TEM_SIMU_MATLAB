3e3543e6a37b9d6523ffdee7a0aa4aa6cb4f7eb6
Francis <warner323@outlook.com>
2019-12-06 16:20:05 +0800

    Adapted some details.
    Made the interfaces more adaptive.

---
+++ Xversion/AnularDetector_X.m
@@ -0,0 +1,35 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Copyright (C) 2019  Francis Black Lee and Li Xian
+
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   any later version.
+
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+%   Email: warner323@outlook.com
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [detector] = AnularDetector_X(LowAngle, HighAngle, WavLen, Lx, Ly, Nx, Ny)
+%AnularDetector_X.m generates the detector for ADF-STEM mode.
+%   LowAngle, HighAngle -- describe the shape of the detector in mrad;
+%   WavLen -- wavelength of the electron beam;
+%   Lx, Ly, Nx, Ny -- sampling parameters;
+% Note: X denotes an experimental version!
+
+dx = Lx / Nx;
+dy = Ly / Ny;
+fx = -1 / (2 * dx) : 1 / Lx : 1 / (2 * dx) - 1 / Lx;
+fy = -1 / (2 * dy) : 1 / Ly : 1 / (2 * dy) - 1 / Ly;
+[FX, FY] = meshgrid(fx, fy);
+FreqSqu = FX.^2 + FY.^2;
+detector = ((FreqSqu < (HighAngle * 1e-3 / WavLen)^2) & (FreqSqu > (LowAngle * 1e-3 / WavLen)^2));
+
+end
+
---
+++ Xversion/HighEnergyWavLen_X.m
@@ -0,0 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Copyright (C) 2019  Francis Black Lee and Li Xian
+
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   any later version.
+
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+%   Email: warner323@outlook.com
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [WavLen] = HighEnergyWavLen_X(KeV)
+%HighEnergyWavLen_X.m calculates high-energy electron beam wavelength.
+%   KeV -- beam energy in KeV;
+% Note: X denotes an experimental version!
+
+WavLen = 12.3986 / sqrt((2 * 511.0 + KeV) * KeV);
+
+end
+
