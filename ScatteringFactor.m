c40beabec41467fe13311a03f5ab860b9bc894d6
Francis <warner323@outlook.com>
2020-03-26 14:38:07 +0800

    Update ScatteringFactor.m

--- ScatteringFactor.m
+++ ScatteringFactor.m
@@ -1,6 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Copyright (C) 2019 - 2020  Francis Black Lee and Li Xian
+
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   any later version.
+
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+%   Email: warner323@outlook.com
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [sf] = ScatteringFactor(AtomType, q)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%Calcuate scattering factors with respect to element type and scattering 
+%angle.
+%   AtomType: type of the atom (atomic number);
+%   q: scattering angle;
 
 FileName = mfilename('fullpath');
 FileName = strcat(FileName, '.m');
