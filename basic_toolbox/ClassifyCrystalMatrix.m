9557e6e07555144e1a686f6d9a762115e7b799b4
Francis <warner323@outlook.com>
2020-09-15 11:49:22 +0800

    Update NanoDiff and its dependent tools

---
+++ basic_toolbox/ClassifyCrystalMatrix.m
@@ -0,0 +1,14 @@
+function [crysMatCell] = ClassifyCrystalMatrix(crystalMatrix)
+%ClassifyCrystalMatrix() classifies the crystal matrix by element types and
+%saves as a cell.
+% Input:
+%   crystalMatrix -- unclassified crystal matrix: [type; prop; x; y; z];
+% Output:
+%   crysMatCell -- cell structure containing classified crystal matrix,
+%       each cell element stands for one type of element and its component
+%       in the raw crystal matrix;
+
+
+
+end
+
