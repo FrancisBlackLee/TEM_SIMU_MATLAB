7a8274c12891604d735854758abdbaebec94e0f9
Francis <warner323@outlook.com>
2023-02-23 15:25:28 +0800

    added WriteEjkXyz

---
+++ basic_toolbox/WriteEjkXyz.m
@@ -0,0 +1,23 @@
+function WriteEjkXyz(filename, lattConsts, typeCoords, wobbles)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+fid = fopen(filename, 'w');
+if fid == -1
+    error('Failed to open file!');
+else
+    fprintf(fid, 'Created by TEM_SIMU_MATLAB\n');
+    fprintf(fid, '\t%.6f\t%.6f\t%.6f\n', lattConsts(1), lattConsts(2),...
+        lattConsts(3));
+    atomNum = size(typeCoords, 2);
+    for atomIdx = 1 : atomNum
+        fprintf(fid, '%d\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\n',...
+            typeCoords(1, atomIdx), typeCoords(3, atomIdx), typeCoords(4, atomIdx),...
+            typeCoords(5, atomIdx), typeCoords(2, atomIdx), wobbles(atomIdx));
+    end
+    fprintf(fid, '-1\n');
+
+    fclose(fid);
+end
+
+end
---
+++ tests/cif_op/test_WriteEjkXyz.m
@@ -0,0 +1,13 @@
+% test_WriteEjkXyz.m
+clc;
+clear;
+close all;
+%% main:
+crysInfo = LoadCif('tests/cif_op/Si_mp-149_symmetrized.cif');
+[cellLengths, cellAngles] = ExtractCellConstFromCrysInfo(crysInfo);
+convMat = ConversionMatrix(cellLengths, cellAngles);
+initAtomSites = ExtractAtomSiteFromCrysInfo(crysInfo);
+atomSites = RemoveSymmetricAtoms(initAtomSites);
+atomSites(3 : 5, :) = convMat * atomSites(3 : 5, :);
+wobbles = zeros(1, size(atomSites, 2));
+WriteEjkXyz('tests/cif_op/test_si100.xyz', cellLengths, atomSites, wobbles);
