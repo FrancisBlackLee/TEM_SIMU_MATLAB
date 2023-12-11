c496da7fb125d5964539e2040d745b96de337ced
Francis <warner323@outlook.com>
2023-12-11 15:54:06 +0800

    Updated qe tests

--- tests/qe/prototype_ph_transition_potential.m
+++ tests/qe/prototype_ph_transition_potential.m
@@ -19,26 +19,30 @@ PlotUnitCell3D(convMat, superCell);
 %% matdyn.modes
 [qs, bands, eigenVecs] = ReadMatdynModes('tests/qe/matdyn.modes');
 thr = 0.2;
-nj = 1;
+nPh = 1;
 
 %% sampling:
 lx = norm(a1) * nks(1);
 ly = norm(a2) * nks(2);
 nx = 512;
 ny = 512;
+x = InitAxis(lx, nx);
+y = InitAxis(ly, ny);
 fx = InitFreqAxis(lx, nx);
 fy = InitFreqAxis(ly, ny);
 [fxMesh, fyMesh] = meshgrid(fx, fy);
 frMesh = sqrt(fxMesh.^2 + fyMesh.^2);
 
 %% transition function:
-iq = 1;
+iq = 169;
 q = qs(iq, :);
 nAtom = size(superCell, 2);
 nBand = size(bands, 2);
 h = zeros(ny, nx);
 hbar = 1.054571817e-34;
 nUnitCellAtom = size(unitCell, 2);
+iBand = 5;
+eLoss = 4.136 * abs(bands(iq, iBand));
 for iAtom = 1 : nAtom
     h1 = exp(-2 * pi * 1i * (fxMesh * carts(1, iAtom) + ...
         fyMesh * carts(2, iAtom))) .* ...
@@ -46,14 +50,14 @@ for iAtom = 1 : nAtom
 
     h2 = ones(ny, nx);
     iUnitCellAtom = mod(iAtom - 1, nUnitCellAtom) + 1;
-    for iBand = 1 : nBand
+    for iPh = 1 : nPh
         if abs(bands(iq, iBand)) > thr
             omega = 2 * pi * bands(iq, iBand) * 1e12;
             dwf = pi^2 * hbar / omega;
             epsilon = 1 / sqrt(nCell) * real(eigenVecs(iUnitCellAtom, :, iBand, iq) * ...
                 exp(2 * pi * 1i * dot(q, fracs(:, iAtom)')));
             qEpsilon = fxMesh * epsilon(1) + fyMesh * epsilon(2);
-            h2 = h2 * (-1i * sqrt(2 * dwf) * qEpsilon).^nj / factorial(nj) .* ...
+            h2 = h2 .* (-1i * sqrt(2 * dwf) * qEpsilon).^nPh / factorial(nPh) .* ...
                 exp(-dwf * qEpsilon.^2);
         end
     end
@@ -61,6 +65,24 @@ for iAtom = 1 : nAtom
 end
 
 %% plot transition probability
-hSqr = abs(h.^2);
+rH = ifftshift(fft2(fftshift(h)));
+rHSqr = abs(rH.^2);
+blurredRhSqr = imgaussfilt(rHSqr, 2);
 figure;
-imagesc(fx, fy, hSqr);
+subplot(1, 2, 1);
+imagesc(x, y, blurredRhSqr);
+colormap('jet');
+axis square;
+xlabel('x (\AA)', 'Interpreter', 'latex');
+ylabel('y (\AA)', 'Interpreter', 'latex');
+title('$|H(\mathbf{r})|^2$', 'Interpreter', 'latex');
+
+hSqr = abs(h.^2);
+blurredHSqr = imgaussfilt(hSqr, 2);
+subplot(1, 2, 2);
+imagesc(fx, fy, blurredHSqr);
+colormap('jet');
+axis square;
+xlabel('$f_x$ (1 / \AA)', 'Interpreter', 'latex');
+ylabel('$f_y$ (1 / \AA)', 'Interpreter', 'latex');
+title('$|H(\mathbf{q})|^2$', 'Interpreter', 'latex');
