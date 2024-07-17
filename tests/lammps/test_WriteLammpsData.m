73d0754a22af5768772f46415362e8018dd92596
Francis <warner323@outlook.com>
2024-07-17 17:12:01 +0800

    Modified test_WriteLammpsData

--- tests/lammps/test_WriteLammpsData.m
+++ tests/lammps/test_WriteLammpsData.m
@@ -10,11 +10,14 @@ unitCell = RemoveSymmetricAtoms(unitCell);
 figure;
 PlotUnitCell3D(convMat, unitCell);
 
-supercell = TileUnitCell(unitCell, [3, 1, 2]);
+nc = [3, 1, 2];
+supercell = TileUnitCell(unitCell, nc);
 
 figure;
 PlotUnitCell3D(convMat, supercell);
 
 supercell(3 : 5, :) = convMat * supercell(3 : 5, :);
 
-WriteLammpsData('tests/lammps/AlGaAs2_3x1x2.lammps', convMat, supercell([1, 3 : 5], :));
+supercellConvMat = [nc(1) * convMat(:, 1), nc(2) * convMat(:, 2), nc(3) * convMat(:, 3)];
+
+WriteLammpsData('tests/lammps/AlGaAs2_3x1x2.lammps', supercellConvMat, supercell([1, 3 : 5], :));
